e530d9688db881a42b750056fb8c523109984ed4
imjohnzakkam <johnzakkam2592@gmail.com>
2020-04-10 19:44:09 +0530

    update cp.sh

--- cp_snippets/cp.sh
+++ cp_snippets/cp.sh
@@ -5,3 +5,41 @@ cc() {
 }
 #execute cpp-file
 alias o="./a.out"
+
+
+cf() {
+#you can change the path
+mkdir ~/Desktop/Codeforces/$1
+cd ~/Desktop/Codeforces/$1
+#test.cpp is my template file
+cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+mv test.cpp a.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+mv test.cpp b.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+mv test.cpp c.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+mv test.cpp d.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+mv test.cpp e.cpp
+}
+```
+```shell
+#similarly for codechef
+cc() {
+#you can change the path
+mkdir ~/Desktop/Codechef/$1
+cd ~/Desktop/Codechef/$1
+#test.cpp is my template file
+cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
+mv test.cpp 1.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
+mv test.cpp 2.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
+mv test.cpp 3.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
+mv test.cpp 4.cpp
+cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
+mv test.cpp 5.cpp
+}
+```
