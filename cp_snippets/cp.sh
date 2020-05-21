245aca7bbc2e1b3f37c0685d36da8d67e9a1fc02
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-21 19:17:53 +0530

    u

--- cp_snippets/cp.sh
+++ cp_snippets/cp.sh
@@ -11,17 +11,15 @@ cf() {
 # you can change the path
 mkdir ~/Desktop/Codeforces/$1
 cd ~/Desktop/Codeforces/$1
-# test.cpp is my template file
-cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
-mv test.cpp a.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
-mv test.cpp b.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
-mv test.cpp c.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
-mv test.cpp d.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
-mv test.cpp e.cpp
+loop=0 
+var=( {a..z} )
+while [ $loop -lt $2 ] 
+do
+	# test.cpp is my template file
+	cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+	mv test.cpp ${var[a]}.cpp
+	loop=`expr $loop + 1` 
+done 
 }
 ```
 ```shell
@@ -30,16 +28,14 @@ cc() {
 #you can change the path
 mkdir ~/Desktop/Codechef/$1
 cd ~/Desktop/Codechef/$1
-#test.cpp is my template file
-cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
-mv test.cpp 1.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
-mv test.cpp 2.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
-mv test.cpp 3.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
-mv test.cpp 4.cpp
-cp ~/Desktop/test.cpp ~/Desktop/Codechef/$1 
-mv test.cpp 5.cpp
+loop=0 
+var=( {a..z} )
+while [ $loop -lt $2 ] 
+do
+	# test.cpp is my template file
+	cp ~/Desktop/test.cpp ~/Desktop/Codeforces/$1 
+	mv test.cpp ${var[a]}.cpp
+	loop=`expr $loop + 1` 
+done 
 }
 ```
